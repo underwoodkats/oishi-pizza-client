a822a2deef0babffedd5f35b4ecf9d23f86dadc3
Igor Katselenbogen <underwoodkats@gmail.com>
2020-01-15 21:44:47 +0900

    Created cart page

---
+++ pages/cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Cart</h1>
+    {{ cart }}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'Cart',
+  computed: mapState({ cart: (state) => state.restaurant.cart })
+}
+</script>
+
+<style scoped></style>
