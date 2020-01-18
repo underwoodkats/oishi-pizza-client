73bbaeb94533849f13371d58e76a8625352a7faa
Igor Katselenbogen <underwoodkats@gmail.com>
2020-01-18 13:43:01 +0900

    Displaying cart content on the cart page

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,16 +1,36 @@
 <template>
-  <div>
+  <div class="cart__page">
     <h1>Cart</h1>
-    {{ cart }}
+    <div class="cart__container">
+      <cart-record
+        v-for="(cartRecord, index) in cart"
+        :key="index"
+        :cartRecord="cartRecord"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import CartRecord from '~/components/CartRecord'
+
 export default {
   name: 'Cart',
+  components: {
+    CartRecord
+  },
   computed: mapState({ cart: (state) => state.restaurant.cart })
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.cart__page {
+  padding: 20px;
+}
+.cart__container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
