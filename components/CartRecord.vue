48a60627a46309ec89a946073d3f486bdf9b9947
Igor Katselenbogen <underwoodkats@gmail.com>
2020-01-18 13:37:13 +0900

    Created component to display items in the cart on the cart page

---
+++ components/CartRecord.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="cart-record__row -shadow">
+    <div class="cart-record__img">
+      <img :src="getImagePath()" />
+    </div>
+    <div class="cart-record__title">
+      <h4>{{ cartRecord.title }}</h4>
+    </div>
+    <div class="cart-record__amount-control">
+      <div>
+        <button @click="addMore" type="button" class="cart-record__icon">
+          -
+        </button>
+      </div>
+      <div>
+        <h5>{{ cartRecord.amount }}</h5>
+      </div>
+      <div>
+        <button @click="addMore" type="button" class="cart-record__icon">
+          +
+        </button>
+      </div>
+    </div>
+    <div>
+      <h5>{{ cartRecord.priceDollar }}$</h5>
+    </div>
+    <div>
+      <button @click="addMore" type="button" class="cart-record__icon">
+        X
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartRecord',
+  props: {
+    cartRecord: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    getImagePath() {
+      return (
+        'http://localhost:8080/resources/image/' + this.cartRecord.imagePath
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+h5,
+h6 {
+  font-size: 18px;
+  margin: 0;
+}
+h6 {
+  font-weight: 100;
+}
+.cart-record__row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  height: auto;
+  width: 50%;
+  padding: 20px;
+  margin: 20px 0;
+}
+.cart-record__img {
+  max-width: 15%;
+  height: auto;
+}
+.cart-record__title {
+  min-width: 40%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.cart-record__amount-control {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.cart-record__icon {
+  margin: 10px;
+  display: flex;
+  flex-direction: row;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+}
+</style>
